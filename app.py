9051721204f5363fadc603b0c07a1d0c190c629f
Tdanner91 <33235253+Tdanner91@users.noreply.github.com>
2018-09-21 20:01:57 -0400

    Added Runtime.txt

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Hello World!"
+    return render_template('ThisIsJustATest.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
